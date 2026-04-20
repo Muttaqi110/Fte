bbcc9f10ba703ab45dee4f1f548995daf847fe42
Muttaqi110 <muttaqiqamar@gmail.com>
2026-04-20 15:25:30 +0500

    removed ui

--- fte-ui/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-module.exports = {
-  allowedDevOrigins: ['192.168.0.104'],
-}
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
